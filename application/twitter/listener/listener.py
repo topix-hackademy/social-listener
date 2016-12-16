b77d41e32af4dee6bb0efd33d25550ba2d98b66e
alexcomu <alex.comunian@gmail.com>
2016-12-16 12:53:44 +0100

    fixed file location error

--- application/twitter/listener/listener.py
+++ application/twitter/listener/listener.py
@@ -1,8 +1,6 @@
-import logging
-
 import tweepy
-from application.mongo import Connection
-from application.twitter.listener.interface import TwitterInterface
+from application.twitter.listener.streaming import TwitterStreamingListener
+from application.twitter.interface import TwitterInterface
 
 
 class TwitterListener(TwitterInterface):
@@ -35,29 +33,3 @@ class TwitterListener(TwitterInterface):
         return "Twitter Interface <{hashtags}>".format(auth=self.auth,
                                                        hashtags=self.hashtags)
 
-
-class TwitterStreamingListener(tweepy.StreamListener):
-
-    def __init__(self):
-        super(TwitterStreamingListener, self).__init__()
-
-    def on_status(self, tweet):
-        """
-        If new tweet is received, this function will be called.
-        For each tweet will create a new Mongo Insert
-        :param tweet:
-        :return:
-        """
-        Connection.Instance().insert('twitter',
-                                     {
-                                         "data": tweet._json,
-                                         "hashtags": tweet.entities['hashtags']
-                                     })
-
-    def on_error(self, status_code):
-        """
-        Error received
-        :param status_code: Status Error Code
-        :return:
-        """
-        logging.error("Twitter Error: ", status_code)
