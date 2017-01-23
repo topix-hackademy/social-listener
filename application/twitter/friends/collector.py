378899b2432345ccd99313e4a43466be2405f464
alexcomu <alex.comunian@gmail.com>
2017-01-23 12:36:16 +0100

    avoid duplicates in get friends

--- application/twitter/friends/collector.py
+++ application/twitter/friends/collector.py
@@ -44,26 +44,29 @@ class FriendsCollector(TwitterInterface):
         """
         for friend in self.fetcherInstance.get_friends():
             try:
-                Connection.Instance().db.twitter.insert_one({
-                    'source': 'friends',
-                    'data': {
-                        'userid': friend.id,
-                        'description': friend.description,
-                        'favourites_count': friend.favourites_count,
-                        'followers_count': friend.followers_count,
-                        'friends_count': friend.friends_count,
-                        'lang': friend.lang,
-                        'location': friend.location,
-                        'name': friend.name,
-                        'screen_name': friend.screen_name,
-                        'geo_enabled': friend.geo_enabled,
-                        'url': friend.url,
-                        'time_zone': friend.time_zone,
-                        'statuses_count': friend.statuses_count
-                    },
-                    'user': self.user,
-                    'created': what_time_is_it()
-                })
+                if not Connection.Instance().db.twitter.find_one({'source': 'friends',
+                                                                  'user': self.user,
+                                                                  'data.userid': friend.id}):
+                    Connection.Instance().db.twitter.insert_one({
+                        'source': 'friends',
+                        'data': {
+                            'userid': friend.id,
+                            'description': friend.description,
+                            'favourites_count': friend.favourites_count,
+                            'followers_count': friend.followers_count,
+                            'friends_count': friend.friends_count,
+                            'lang': friend.lang,
+                            'location': friend.location,
+                            'name': friend.name,
+                            'screen_name': friend.screen_name,
+                            'geo_enabled': friend.geo_enabled,
+                            'url': friend.url,
+                            'time_zone': friend.time_zone,
+                            'statuses_count': friend.statuses_count
+                        },
+                        'user': self.user,
+                        'created': what_time_is_it()
+                    })
             except Exception as e:
                 logging.error("MongoDB Insert Error in get friends: " + e.message)
         import multiprocessing
